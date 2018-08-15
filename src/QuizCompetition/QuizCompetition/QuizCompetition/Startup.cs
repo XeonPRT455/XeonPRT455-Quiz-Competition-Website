d3cbd5bc717d3bd30141eaad63c10da71fbfa78c
aymwxbb2012 <liangchaotech@gmail.com>
2018-08-15 16:03:54 +0930

    snapshot 0.01

---
+++ src/QuizCompetition/QuizCompetition/QuizCompetition/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizCompetition.Startup))]
+namespace QuizCompetition
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
